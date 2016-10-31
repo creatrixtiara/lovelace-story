6ceaf2620e6958ca74db64d060bc2d0dd21b57b3
Mark Cottman-Fields <mark.cottmanfields@qut.edu.au>
2016-10-31 20:56:59 +1000

    fixed up bracket confusion

--- js/main.js
+++ js/main.js
@@ -19,19 +19,16 @@ data = [
     }
 ];
 
-$(document).ready(function () {
-    toggleFields(); //call this first so we start out with the correct visibility depending on the selected form values
-    //this will call our toggleFields function every time the selection value of our underAge field changes
-    $('#answer').change(function () {
-        toggleFields();
-    });
-    
+
+// this function decides which data item to show
 function render(){
     suffix = "==";
     $('#question').text(atob(data[0]["question_value"]+suffix));
 
     $('#answer').text();
-} 
+}
+
+// this function checks if an answer is correct
 function toggleFields(){
     if ($('#answer').atob(val()+suffix) == (data[0]["answer_value"]+suffix)) {
         $('#story').text(atob(data[0]["story_value"]+suffix));
@@ -41,8 +38,15 @@ function toggleFields(){
         $('#story').hide()
     }
 }
-}
 
-$( document ).ready(function() {
+// this function sets up things so the page actually does things
+$(document).ready(function () {
+
     render();
-};
+
+    toggleFields(); //call this first so we start out with the correct visibility depending on the selected form values
+    //this will call our toggleFields function every time the selection value of our underAge field changes
+    $('#answer').change(function () {
+        toggleFields();
+    });
+});
