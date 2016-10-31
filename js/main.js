09c48e5ea978e259d7b6a0a302c3e3463bd69763
Creatrix Tiara <me@creatrixtiara.com>
2016-10-31 21:47:48 +1100

    adding toggleFields
    http://jsfiddle.net/jadendreamer/5SArB/

--- js/main.js
+++ js/main.js
@@ -19,12 +19,20 @@ data = [
     }
 ];
 
+$(document).ready(function () {
+    toggleFields(); //call this first so we start out with the correct visibility depending on the selected form values
+    //this will call our toggleFields function every time the selection value of our underAge field changes
+    $('#answer').change(function () {
+        toggleFields();
+    });
+    
 function render(){
     suffix = "==";
     $('#question').text(atob(data[0]["question_value"]+suffix));
 
     $('#answer').text();
     
+function toggleFields(){
     if ($('#answer').atob(val()+suffix) == (data[0]["answer_value"]+suffix)) {
         $('#story').text(atob(data[0]["story_value"]+suffix));
         $('#story').show();
@@ -32,6 +40,7 @@ function render(){
     else {
         $('#story').hide()
     }
+}
 }
 
 $( document ).ready(function() {
