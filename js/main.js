d715d74fb141e41c129ffadddd2ce0cae35b7a27
Creatrix Tiara <me@creatrixtiara.com>
2016-11-01 16:31:59 +1100

    updated questions, loren ipsum

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 data = [
     {
-        "date": "2016-10-31",
+        "question_number": "0",
         "question_value": "V2hhdCBpcyAxICsxPw",
         "question_type": "text",
         "answer_value": "Mg",
@@ -9,13 +9,13 @@ data = [
         "story_type": "text"
     },
     {
-        "date": "2016-10-31",
+        "question_number": "1",
         "question_value": "V2hhdCBpcyAxICsxPw",
         "question_type": "text",
         "answer_value": "Mg",
         "answer_type": "text",
-        "story_value": "T25jZSB1cG9uIGEgdGltZS4uLg",
-        "story_type": "text"
+        "story_value": "PHA+TG9yZW0gaXBzdW0gZG9sb3Igc2l0IGFtZXQsIGNvbnNlY3RldHVyIGFkaXBpc2NpbmcgZWxpdC4gRG9uZWMgbm9uIGdyYXZpZGEgZXJhdC4gQWVuZWFuIG5vbiBsaWJlcm8gZGFwaWJ1cywgdGluY2lkdW50IGFudGUgZXVpc21vZCwgZWZmaWNpdHVyIHRlbGx1cy4gRnVzY2UgZXUgcGVsbGVudGVzcXVlIG5pYmguIFNlZCBzZW1wZXIgbWF4aW11cyBvZGlvLCBpZCBjb25kaW1lbnR1bSBtYWduYSBzb2xsaWNpdHVkaW4gdXQuIEFlbmVhbiBtb2xsaXMgbnVuYyBkaWN0dW0gY3Vyc3VzIHNvbGxpY2l0dWRpbi4gUHJvaW4gZmV1Z2lhdCBtYXNzYSBldSByaG9uY3VzIHJ1dHJ1bS4gTnVsbGEgcXVpcyBsaWJlcm8gYWMgYW50ZSBwb3J0YSBmaW5pYnVzIHZlbCB2ZWwgZXJhdC4gVmVzdGlidWx1bSBlZ2V0IHJpc3VzIHNpdCBhbWV0IG51bGxhIGxvYm9ydGlzIGN1cnN1cy4gU3VzcGVuZGlzc2Ugc2l0IGFtZXQgaW1wZXJkaWV0IHVybmEsIHNlZCBtb2xsaXMgZXJhdC4gQWxpcXVhbSBldSBkdWkgbm9uIGxhY3VzIGxvYm9ydGlzIGxhb3JlZXQuIFZlc3RpYnVsdW0gZXUganVzdG8gZGljdHVtLCBzdXNjaXBpdCBuaXNsIHF1aXMsIGdyYXZpZGEgc2VtLiBNb3JiaSBlZmZpY2l0dXIgcGxhY2VyYXQgZXgsIGV0IGxhY2luaWEgbGFjdXMgdmVzdGlidWx1bSBub24uIFZpdmFtdXMgdmVoaWN1bGEsIGlwc3VtIHZlbCBmaW5pYnVzIHZlc3RpYnVsdW0sIG5pYmggYXVndWUgZ3JhdmlkYSBuaXNpLCBzZWQgY29uc2VxdWF0IG1pIHVybmEgdXQgbWV0dXMuPC9wPg0KDQo8cD5WZXN0aWJ1bHVtIGRpY3R1bSBkYXBpYnVzIG5pYmggc2l0IGFtZXQgZWZmaWNpdHVyLiBBZW5lYW4gZWZmaWNpdHVyIG1hdXJpcyBxdWlzIGVzdCBmZXVnaWF0LCBlZ2V0IGNvbnZhbGxpcyByaXN1cyBtYWxlc3VhZGEuIEludGVnZXIgdGVtcG9yIHZhcml1cyBpcHN1bSwgaW4gY29uZ3VlIGlwc3VtIHB1bHZpbmFyIHZlaGljdWxhLiBJbiBzZWQgdG9ydG9yIG5lYyBqdXN0byB2ZXN0aWJ1bHVtIGFsaXF1YW0gYWMgdXQgdXJuYS4gQ3VyYWJpdHVyIG5pYmggZXN0LCBiaWJlbmR1bSBpZCBuZXF1ZSBncmF2aWRhLCBncmF2aWRhIHZpdmVycmEgbGliZXJvLiBDcmFzIGNvbmd1ZSB0ZWxsdXMgaW4gZXggZXVpc21vZCwgZXUgZXVpc21vZCBwdXJ1cyBtb2xsaXMuIFBoYXNlbGx1cyB1bGxhbWNvcnBlciBlcmF0IHNhcGllbiwgYWMgY29uZ3VlIG5pYmggcG9ydHRpdG9yIGEuIFV0IG5vbiBsaWJlcm8gdXJuYS4gU2VkIHN1c2NpcGl0IGxvcmVtIGFjY3Vtc2FuIGVsaXQgcG9zdWVyZSwgdml0YWUgdGluY2lkdW50IGVsaXQgZGljdHVtLiBOdWxsYSBlbGVtZW50dW0gc2VtIHF1aXMgaXBzdW0gZWxlbWVudHVtIHVsdHJpY2VzLiBQcmFlc2VudCBldCBpbnRlcmR1bSBqdXN0by4gQWVuZWFuIGlkIG1hZ25hIG1pLiBWZXN0aWJ1bHVtIGEgc3VzY2lwaXQgbGlndWxhLCBzZWQgZWxlbWVudHVtIGVyYXQuPC9wPg0KDQo8cD5BbGlxdWFtIGVyYXQgdm9sdXRwYXQuIEFlbmVhbiBoZW5kcmVyaXQgbWFzc2EgYWMgYXVndWUgbWF0dGlzLCBpZCBkaWN0dW0gdHVycGlzIHNjZWxlcmlzcXVlLiBGdXNjZSBuZWMgbWkgZW5pbS4gRG9uZWMgYmxhbmRpdCB2ZXN0aWJ1bHVtIHN1c2NpcGl0LiBOdWxsYSBvcm5hcmUgbGVvIHNlZCBwdWx2aW5hciBhbGlxdWFtLiBDbGFzcyBhcHRlbnQgdGFjaXRpIHNvY2lvc3F1IGFkIGxpdG9yYSB0b3JxdWVudCBwZXIgY29udWJpYSBub3N0cmEsIHBlciBpbmNlcHRvcyBoaW1lbmFlb3MuIE51bmMgc2FnaXR0aXMgc2VkIGxvcmVtIGV1IHNlbXBlci48L3A+",
+        "story_type": "html"
     }
 ];
 
